939c6bd3b1c393ab7b09c0f111a259e715a1aa35
春内悠甫 <>
2021-07-05 14:59:03 +0900

    質問に対する返信を引数として受け取れるようにした

---
+++ main.js
@@ -0,0 +1,105 @@
+(function() {
+
+  var url = 'https://api.github.com/search/repositories?q=';
+  var msgIndex, key;
+  var botui = new BotUI('chat');
+
+
+  //初期メッセージ
+  botui.message.bot({
+    content: 'こんにちは！'
+  }).then(init);
+
+
+  function init() {
+    botui.message.bot({
+      delay: 1500,  //メッセージの表示タイミングをずらす
+      content: '気になるキーワードで、GitHubの総リポジトリ数をお答えします！'
+    }).then(function() {
+
+      //キーワードの入力
+      //「return」を記述して、ユーザーからの入力待ち状態にする
+      return botui.action.text({
+        delay: 1000,
+        action: {
+          placeholder: '例：javascript'
+        }
+      });
+    }).then(function(res) {
+
+      //入力されたキーワードを取得する
+      key = res.value;
+      getRepositories(key);
+
+      //ローディング中のアイコンを表示
+      botui.message.bot({
+        loading: true
+      }).then(function(index) {
+
+        //ローディングアイコンのindexを取得
+        //このindexを使ってメッセージ情報を更新する
+        //（更新しないとローディングアイコンが消えないため…）
+        msgIndex = index;
+      });
+    });
+  }
+
+
+  //GitHubのリポジトリを取得する処理
+  function getRepositories(keyword) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url + keyword);
+    xhr.onload = function() {
+      var result = JSON.parse(xhr.responseText);
+
+      var answer_message = "質問ありがとう"
+      //取得したリポジトリ数をshowMessage()に代入する
+      showMessage(answer_message);
+    }
+    xhr.send();
+  }
+
+
+  //リポジトリ総数をメッセージに表示する処理
+  function showMessage(answer_message) {
+
+    //ローディングアイコンのindexを使ってメッセージを書き換える
+    botui.message.update(msgIndex, {
+      content: answer_message
+    }).then(function() {
+      return botui.message.bot({
+        delay: 1500,
+        content: 'まだ続けますか？'
+      })
+    }).then(function() {
+
+      //「はい」「いいえ」のボタンを表示
+      return botui.action.button({
+        delay: 1000,
+        action: [{
+          icon: 'circle-thin',
+          text: 'はい',
+          value: true
+        }, {
+          icon: 'close',
+          text: 'いいえ',
+          value: false
+        }]
+      });
+    }).then(function(res) {
+
+      //「続ける」か「終了」するかの条件分岐処理
+      res.value ? init() : end();
+    });
+  }
+
+
+  //プログラムを終了する処理
+  function end() {
+    botui.message.bot({
+      content: 'ご利用ありがとうございました！'
+    })
+  }
+
+})();
